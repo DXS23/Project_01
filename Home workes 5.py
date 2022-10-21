e4a42a75cd006e2ef5c5a5a4f82354845b0727a4
DXS23 <114493399+DXS23@users.noreply.github.com>
2022-10-21 11:14:44 +0300

    Add files via upload

---
+++ Home workes 5.py
@@ -0,0 +1,8 @@
+salary, expenses = 10000, 12000
+salary *= 12   #зарплата за 12 месяцев
+sp = [12000]   # список с январём
+for i in range(2,12):
+    expenses += expenses * 0.03    # увеличение расхода каждый месяц от уже увеличенных расходов
+    sp.append(expenses)
+credit = sum(sp) - salary     # разница зарлаты и расходов за год
+print('Необходимо взять в долг', round(credit, 2), 'рублей')
