1b622d4d6635acf9a2fa886ac2f0cba3bf27bc93
DXS23 <114493399+DXS23@users.noreply.github.com>
2022-10-24 14:18:20 +0300

    Add files via upload

---
+++ Home workes 6.py
@@ -0,0 +1,17 @@
+import random
+my_favorite_songs = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+songs = list(my_favorite_songs.items())
+songs = random.sample(songs, 3)
+time = (songs[0][1] + songs[1][1] + songs[2][1])
+print(*songs)
+print('Три песни звучат:', round(time, 2), 'минут.')
