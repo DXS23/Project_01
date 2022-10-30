bf3259047d1da3375e2984efc488b1ab37064c63
DXS23 <114493399+DXS23@users.noreply.github.com>
2022-10-30 11:09:36 +0300

    Update Home workes 5.py
    Поправлен месяц 12 включить

--- Home workes 5.py
+++ Home workes 5.py
@@ -1,7 +1,7 @@
 salary, expenses = 10000, 12000
 salary *= 12   #зарплата за 12 месяцев
 sp = [12000]   # список с январём
-for i in range(2,12):
+for i in range(2,13):
     expenses += expenses * 0.03    # увеличение расхода каждый месяц от уже увеличенных расходов
     sp.append(expenses)
 credit = sum(sp) - salary     # разница зарлаты и расходов за год
